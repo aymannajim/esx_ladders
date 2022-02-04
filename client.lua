6a68b8fa0b1cb214780d762ce6ff83ad14c5dfc8
AN <36258300+notaymanTV@users.noreply.github.com>
2022-02-04 01:13:06 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,3 @@
--- Inferno Collection Ladders Version 1.3 Beta
---
 -- Copyright (c) 2019-2020, Christopher M, Inferno Collection. All rights reserved.
 --
 -- This project is licensed under the following:
@@ -7,8 +5,7 @@
 -- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -- THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. THE SOFTWARE MAY NOT BE SOLD.
 --
--- This script is an edited version of Inferno Collection Ladders Version 1.3 Beta
--- Credits goes to: https://github.com/inferno-collection/Ladders
+-- This script is an edited version of Inferno Collection Ladders
 
 ESX              = nil
 local PlayerData = {}
